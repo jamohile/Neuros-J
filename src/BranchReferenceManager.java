1cdecf46a62e5c4afaaef52092e39bf84d13ab7e
Jay Mohile <jamohile@gmail.com>
2017-06-18 08:44:40 -0400

    update

--- src/BranchReferenceManager.java
+++ src/BranchReferenceManager.java
@@ -1,6 +1,9 @@
+import java.util.Iterator;
 import java.util.Vector;
 
 public class BranchReferenceManager {
+	static int lowestStrength;
+	static int highestStrength; // used to caluclate cutoff values for trimming
 	int totalStrength;
 	Vector<Branch> branches;
 
@@ -8,24 +11,31 @@ public class BranchReferenceManager {
 		// TODO Auto-generated constructor stub
 		branches = new Vector<Branch>();
 	}
+
 	public void incrementBranch(Neuron neuron) {
 		incrementBranch(neuron, 1);
 	}
+
 	public void incrementBranch(Neuron neuron, int strength) {
 		int indexOfNeuron = indexOfBranchToNeuron(neuron); // stores the index, or
 															// "-1" if not contained
 		if (indexOfNeuron != -1) {
 			// already contains neuron reference, therefore strengthen the connection
 			branches.elementAt(indexOfNeuron).strengthen(strength);
+			if (branches.elementAt(indexOfNeuron).strength > highestStrength) {
+				highestStrength = branches.elementAt(indexOfNeuron).strength;
+			}
 		} else {
 			createBranch(neuron, strength);
 		}
-		totalStrength += 1; // no matter what happened, the total strength was
-							// increased by one
+		totalStrength += strength; // no matter what happened, the total strength was
+		// increased by one
 	}
+
 	public Branch getBranchByStrength(int strength) {
 		return searchByStrength(0, branches.size() - 1, strength);
 	}
+
 	public Branch searchByStrength(int lower, int upper, int goal) { // recursive
 																		// binary-tree
 																		// search to
@@ -36,7 +46,7 @@ public class BranchReferenceManager {
 																		// argument
 		int midpoint = (int) Math.ceil((lower + upper) / 2);
 		if (midpoint == upper && midpoint == lower) { // search is complete
-			System.out.println(midpoint);
+			// System.out.println(midpoint);
 			return branches.elementAt(midpoint);
 		}
 		// } else {
@@ -47,6 +57,7 @@ public class BranchReferenceManager {
 		}
 		return (searchByStrength(lower, upper, goal));
 	}
+
 	public int getStrengthSum(int upper) {
 		int sum = 0;
 		for (int index = upper; index >= 0; index--) {
@@ -54,16 +65,45 @@ public class BranchReferenceManager {
 		}
 		return sum;
 	}
+
 	public int getSize() {
 		return branches.size();
 	}
+
 	public int getTotalStrength() {
 		return totalStrength;
 	}
+
+	public float getAverageStrength() { // returns the "average" branch strength
+		return (float) totalStrength / (float) (branches.size() + 1);// add one to
+	} // compensate for 0
+
 	public void createBranch(Neuron neuron, int strength) {
 		Branch branch = new Branch(neuron, strength);
 		branches.add(branch);
 	}
+
+	public void removeBranch(Branch branch) {
+		totalStrength -= branch.strength;
+		branches.remove(branch);
+	}
+
+	public void hardTrim() {
+		int trimmed = 0;
+		Iterator<Branch> iterator = branches.iterator();
+		while (iterator.hasNext()) {
+			Branch branch = iterator.next();
+			int branchStrength = branch.getStrength();
+			if (branchStrength < getAverageStrength() && highestStrength > 10 && branchStrength != totalStrength) {
+				// TODO: add random save probability later
+				totalStrength -= branchStrength;
+				iterator.remove();
+				trimmed += 1;
+			}
+		}
+		// System.out.println(trimmed);
+	}
+
 	public boolean containsBranchToNeuron(Neuron neuron) {
 		return (indexOfBranchToNeuron(neuron) != -1) ? true : false; // returns true
 																		// if the
@@ -72,12 +112,15 @@ public class BranchReferenceManager {
 																		// is not
 																		// "-1"
 	}
+
 	public int indexOfBranchToNeuron(Neuron neuron) { // return the index of the
 														// branch containing
 														// reference to argument
 														// neuron, "-1" means
 														// reference is not contained
-		for (Branch branch : branches) {
+		Iterator<Branch> iterator = branches.iterator();
+		while (iterator.hasNext()) {
+			Branch branch = iterator.next();
 			if ((branch.getNeuron()).equals(neuron)) { // equal, therefore it is
 														// contained
 				return branches.indexOf(branch);
