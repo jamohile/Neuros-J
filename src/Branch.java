71d3e09c4a7331357bb664f0f7a8ce0f21548f01
Jay Mohile <jamohile@gmail.com>
2017-06-18 22:17:28 -0400

    comment cleanup

--- src/Branch.java
+++ src/Branch.java
@@ -1,11 +1,13 @@
 public class Branch {
+	//Used to ensure unique id across all branch instances.
 	static int LastID;
-	Neuron neuron;
-	int strength = 1;
 	Integer id;
+	//The neuron this connection is TO
+	Neuron neuron;
+	//The strength of this connection, by default 1
+	int strength = 1;
 
 	public Branch(Neuron destinationNeuron, int strength) {
-		// TODO Auto-generated constructor stub
 		this.id = LastID + 1;
 		LastID = this.id;
 		this.neuron = destinationNeuron;
@@ -18,15 +20,17 @@ public class Branch {
 	public int getStrength() {
 		return strength;
 	}
+	//overload to increase strength by 1
 	public void strengthen() {
 		this.strength += 1;
 	}
+	//increase strength of connection by arbitrary amount
 	public void strengthen(int increment) {
 		this.strength += increment;
 	}
+	//Generates string represenation of branch
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return "" + String.valueOf(neuron.getId() + ":" + strength);
 	}
 }
