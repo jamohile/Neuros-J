02a128298677da4d0acf4dff20cf4dea9de7bb22
Jay Mohile <jamohile@gmail.com>
2017-06-19 10:03:20 -0400

    commenting

--- src/ComplexCharge.java
+++ src/ComplexCharge.java
@@ -1,20 +1,29 @@
 import java.util.Random;
 
 public class ComplexCharge extends Charge implements Runnable {
-	static int total_phases; // how many phases have all complex charges run?
+	//The total number of phases run by all complex charges
+	static int total_phases;
+	//The number of complex charges currently running
 	static int running_charges;
-	int phases; // how many phases has this charge run?
-	long startTime; // when did this start?
-	long runningTime; // how long has this charge been running for? stored in ms, updated once each phase.
-	int stability = 1000000; // how likely is this charge to randomly collapse? stability ranges from 1 - 1 million, lower = more likely to collapse
-	int excitement = 2; // how likely is this charge to randomly split? out of one million
-
-	enum CHARGE_LIMITS {// by what method will this charge die? induvidual phase, total phase, lifetime...
+	//The number of phases this charge has running
+	int phases; 
+	//The start time of this charge in ms
+	long startTime;
+	//the total runtime of this charge, updated each phase
+	long runningTime;
+	//The inverted likeliness of charge to collapse
+	int stability = 1000000; 
+	//likeliness of charge to split
+	int excitement = 2; 
+	//The limiting factor for this charge's life
+	enum CHARGE_LIMITS {
 		PERSONAL_PHASE, TOTAL_PHASE, LIFETIME
 	}
-
+	//The limiting factor used by this charge instance
 	CHARGE_LIMITS limitingFactor;
+	//the time limit for this charge, if lifetime limited
 	long limit;
+	//The thread on which this charge will run
 	Thread thread;
 
 	public ComplexCharge(Neuron start, CHARGE_LIMITS limiting_factor, long limit) {
@@ -26,35 +35,37 @@ public class ComplexCharge extends Charge implements Runnable {
 		startTime = System.currentTimeMillis();
 		running_charges += 1;
 	}
-
+	//all charge runtime will happen in this threaded function
 	public void run() {
-		// TODO Auto-generated method stub
 		while (shouldRun()) {
 			// charge activities
 			while (currentLocation.hasCharge) {
+				//prevent charge from moving to quickly
 				try {
 					thread.sleep(10);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 			// if the phase is synced up with "NEW NEURON EVERY" add a new neuron
 			if (phases % Neuros.NEURON_EVERY == 0) {
-				Neuron neuron = Neuros.addNewNeuron(this.getCurrentLocation()); // adds a new neuron, stemming from current location
+				Neuron neuron = Neuros.addNewNeuron(this.getCurrentLocation());
 				if (Neuros.ARTIFICIALLY_MOVE_CHARGE) {
-					this.move(neuron);// artifically move the charge to the new neuron
+					this.move(neuron);
 				}
 			} else {
 				this.move();
 			}
-			randomActions(); // calculate whether to run random events such as collapse, split, etc.
+			//Caculate the probability of random events such as collapse and split
+			randomActions();
+			//update markers
 			phases += 1;
 			total_phases += 1;
 			runningTime = System.currentTimeMillis() - startTime;
 		}
 		removeCharge();
 	}
+	//remove this charge, and close netwrok if all charges are removed
 	@Override
 	public void removeCharge() {
 		System.out.println("Charge &" + this.id + " is being removed.");
@@ -64,7 +75,7 @@ public class ComplexCharge extends Charge implements Runnable {
 		}
 		Neuros.charges.remove(this);
 	}
-
+	//Calculate the probability of random events, to add spontanaiety.
 	public void randomActions() {
 		Random random = new Random();
 		int probabilitySplit = random.nextInt(1000000);
@@ -79,18 +90,18 @@ public class ComplexCharge extends Charge implements Runnable {
 			removeCharge();
 		}
 	}
-
+	//create and setup a new instance of the complex charge, overloaded for root neuron
 	public static void createComplexCharge() {
 		createComplexCharge(Neuros.neurons.elementAt(0));
 	}
-
+	//create and setup complex charge at arbitrary neuron
 	public static void createComplexCharge(Neuron neuron) {
 		ComplexCharge complexCharge = new ComplexCharge(Neuros.neurons.elementAt(0),
 				ComplexCharge.CHARGE_LIMITS.LIFETIME, 2000);
 		Neuros.charges.add(complexCharge);
 		complexCharge.start();
 	}
-
+	//start the thread, activating the neuron
 	public void start() {
 		if (thread == null) {
 			System.out.println("Charge &" + this.id + " has started running.");
@@ -98,10 +109,8 @@ public class ComplexCharge extends Charge implements Runnable {
 			thread.start();
 		}
 	}
-
+	// keeps track of whether the charge should still be running
 	private boolean shouldRun() {
-		// keeps track of whether the charge should still be running, based on basic predictable factors such as time and phases.
-		// TODO add spontaneous death or recovery
 		switch (limitingFactor) {
 		case PERSONAL_PHASE:
 			if (phases <= limit) {
